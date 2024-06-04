32698006f5ecb97d7069057b300d2792cb932c55
Dikshant Singh <dikshantsingh2001@gmail.com>
2024-06-04 23:18:16 +0530

    Setup create, update, and delete forms along with login and logout functionality

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import TaskList, Task
+from .views import TodoList, TodoDetail, CreateTodo, UpdateTodo, DeleteTodo, CustomLoginView
+
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
-    path('', TaskList.as_view(), name="todos"),
-    path('todo/<uuid:pk>/', Task.as_view(), name="todo")
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+    path("", TodoList.as_view(), name="todos"),
+    path("todo/<uuid:pk>/", TodoDetail.as_view(), name="todo"),
+    path("create-todo/", CreateTodo.as_view(), name="create-todo"),
+    path("update-todo/<uuid:pk>/", UpdateTodo.as_view(), name="update-todo"),
+    path("delete-todo/<uuid:pk>/", DeleteTodo.as_view(), name="delete-todo"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,15 +1,51 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+
 from .models import Todo
 
-class TaskList(ListView):
+
+class CustomLoginView(LoginView):
+    template_name = "base/login.html"
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("todos")
+
+
+class TodoList(ListView):
     model = Todo
     context_object_name = "todos"
     template_name = "base/todos.html"
 
 
-class Task(DetailView):
+class TodoDetail(DetailView):
     model = Todo
     context_object_name = "todo"
-    template_name = "base/todo.html"
+    template_name = "base/todo_detail.html"
+
+
+class CreateTodo(CreateView):
+    model = Todo
+    fields = "__all__"
+    success_url = reverse_lazy("todos")
+    template_name = "base/create_todo.html"
+
+
+class UpdateTodo(UpdateView):
+    model = Todo
+    fields = "__all__"
+    success_url = reverse_lazy("todos")
+    template_name = "base/create_todo.html"
+
+
+class DeleteTodo(DeleteView):
+    model = Todo
+    context_object_name = "todo"
+    success_url = reverse_lazy("todos")
+    template_name = "base/confirm_delete_todo.html"
